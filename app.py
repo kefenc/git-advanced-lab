70658568579f3e1dbfe7369913867ca70aea839c
Biswajit Dash <78814287+biswajit12azure@users.noreply.github.com>
2025-12-16 13:39:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 
-
 app = Flask(__name__)
 
 
@@ -17,4 +16,4 @@ def add_numbers():
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
