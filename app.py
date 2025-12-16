a9df11e142c1e1d7fbde7a22d331a7ecfa768f93
Biswajit Dash <biswajit.azuredevops@gmail.com>
2025-12-16 13:14:57 +0530

    linting changes

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, request
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return jsonify({"message": "Welcome to Py API Demo!"})
+
 
 @app.route('/add', methods=['POST'])
 def add_numbers():
@@ -12,5 +15,8 @@ def add_numbers():
     result = data.get('a', 0) + data.get('b', 0)
     return jsonify({"result": result})
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
