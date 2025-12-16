9b62d6641c777389401071ac71d8f9621b1e8ad9
Biswajit Dash <biswajit.azuredevops@gmail.com>
2025-12-16 11:37:23 +0530

    test files

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+
 import json
 from app import app
 
@@ -11,10 +15,7 @@ def test_home():
 
 def test_add_numbers():
     client = app.test_client()
-    response = client.post(
-        "/add",
-        json={"a": 5, "b": 3}
-    )
+    response = client.post("/add", json={"a": 5, "b": 3})
     data = json.loads(response.data)
 
     assert response.status_code == 200
